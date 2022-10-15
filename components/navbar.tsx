6517516260d5d3909134c305151b75269ab51561
Brijesh-37 <brijeshdudhbhate@gmail.com>
2022-10-15 12:57:10 +0530

    link error solved

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Link from "next/link";
 import styles from "../styles/navbar.module.css";
 
 const Navbar = () => {
@@ -6,9 +7,9 @@ const Navbar = () => {
     <div>
       <nav className={styles.nav0}>
         <ul>
-          <a href="/" className={styles.nav1}>
-            Signup
-          </a>
+          <Link href="/">
+            <a className={styles.nav1}>Signup</a>
+          </Link>
 
           <a href="/user " className={styles.nav2}>
             UserData
